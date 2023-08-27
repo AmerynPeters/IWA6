bd930397bc327c9ab8f922650cb38089b0eb1d93
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-27 12:12:07 +0200

    Fixed Script1

--- script1.js
+++ script1.js
@@ -1,23 +1,33 @@
-
 const primaryPhone = "O748105141";
 const secondaryPhone = "0219131568";
 
 // Only change below this line
 
-const primaryValid = typeof NaN == primaryPhone;
-const secondaryValid = typeof string === secondaryPhone;
-
-
-    if (isNaN(primaryPhone)) {
-    console.log("Primary phone is a valid numerical string:", primaryValid);
+function isNumeric(str) {
+    if (typeof str !== "string") {
+        return false;
+    }
+    return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
-function checkString(string){
-    if (typeof string(secondaryPhone) === "string") {
-        console.log(!isNaN(string));
-    } 
-}
+const primaryValid = isNumeric(primaryPhone);
+const secondaryValid = isNumeric(secondaryPhone);
 
+console.log("Primary phone is valid numerical string:", primaryValid);
+console.log("Secondary phone is valid numerical string:", secondaryValid);
+
+// const primaryValid = typeof NaN == primaryPhone;
+// const secondaryValid = typeof string === secondaryPhone;
+
+//     if (isNaN(primaryPhone)) {
+//     console.log("Primary phone is a valid numerical string:", primaryValid);
+// }
+
+// function checkString(string){
+//     if (typeof string(secondaryPhone) === "string") {
+//         console.log(!isNaN(string));
+//     }
+// }
 
 // parseInt(secondaryPhone)
 // if (typeof primaryValid === NaN) {
@@ -28,27 +38,24 @@ function checkString(string){
 //     console.log("Secondary phone is valid numerical string:", secondaryValid);
 // }
 
-
 // console.log("Primary phone is valid numerical string:", primaryValid);
 // console.log("Secondary phone is valid numerical string:", secondaryValid);
 
 // if(isNaN(primaryPhone)){
 //     console.log("Primary phone is valid numerical string:", primaryValid);
-// } 
+// }
 
 // if(typeof secondaryPhone === ){
-//     console.log("Secondary phone is valid numerical string:", secondaryValid);   
+//     console.log("Secondary phone is valid numerical string:", secondaryValid);
 // }
 
 // if(!isNaN(secondaryPhone)) {
-//     console.log("Secondary phone is valid numerical string:", secondaryValid);   
+//     console.log("Secondary phone is valid numerical string:", secondaryValid);
 // }
-
 
 //   if(typeof primaryPhone === "string"){
 //     console.log("Primary phone is valid numerical string:", primaryValid);
-//   } 
+//   }
 //   if (typeof secondaryPhone === 'String') {
 //       console.log("Primary phone is valid numerical string:", secondaryValid);
 //   }
-
